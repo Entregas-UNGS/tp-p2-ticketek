5a7dd42b422f7bc7d49c46fcf30a26c2870e9930
CristianBrz <cristian.brz.83@gmail.com>
2025-05-19 19:20:11 -0300

    Agrego la clase Entrada e implemento la interfaz IEntrada

---
+++ src/ar/edu/ungs/prog2/ticketek/Entrada.java
@@ -0,0 +1,27 @@
+package ar.edu.ungs.prog2.ticketek;
+
+public class Entrada implements IEntrada{
+  private String codigo;
+  private String nombreEspectaculo;
+  private Fecha fecha;
+  private String sede;
+  private String ubicacion;
+  private double precio;
+//  private Usuario comprador;
+	
+	
+	
+	
+	@Override
+	public double precio() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String ubicacion() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
